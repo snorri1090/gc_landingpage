d7c8e60b6a70fd0ca8a338b45f264aa5a9a9b63c
Scott <scott@monkeyhouse-media.com>
2017-11-02 11:01:44 -0700

    added functionality to cardpop.js and associated markup and styling

--- js/cardpop.js
+++ js/cardpop.js
@@ -4,6 +4,8 @@ jQuery(function($) {
   var cardsContainer;
   var cardPopup = $(".card-popup");
   var activeCard;
+  var cardId;
+  var cardPopupContent;
 
   // select card
   $( card ).click(function() {
@@ -11,38 +13,29 @@ jQuery(function($) {
       // adjacent cards collapse into removed card space
     $( this ).hide();
 
+    // assign name of current card to related popup content
+
     // card pop out animates to center of containing window
       // card pop out displaces other adjacent cards and cards below
-    $( cardPopup ).show();
+      // if cardPopup is already present, it must fade out, then fade back in with new click
+    $( "div" ).closest(cardPopup).fadeIn(200);
+    // add hidden card back if new card is selected
     $( card ).not(this).show();
     activeCard = $(this);
 
 
     // card pop out is populated with the content related to the card clicked
+    cardId = activeCard.attr('class');
+    cardPopupContent = $( "article" ).attr("id", cardId).html();
+    cardPopup.html(cardPopupContent)
   });
 
     // card pop out closes and card returns on click (to be replaced with close button)
   $( cardPopup ).click(function() {
-    $(this).hide();
+    // close window
+    $(this).fadeOut(200);
+    // add hidden card back
     activeCard.show();
 
   });
-
-
-
-
-
-
-
-
-
-
-
-
-  // close window
-
-  // add hidden card back
-
-  // add hidden card back if new card is selected
-
 });
